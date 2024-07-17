daf11caf8c7c735dda7a47f8c2d724e040033cea
riskiai <riskiahmadiilham@gmail.com>
2024-07-17 19:41:49 +0700

    Updated Tgl 17 Juli 2024

--- resources/views/Adminstrator/testimoni/edit.blade.php
+++ resources/views/Adminstrator/testimoni/edit.blade.php
@@ -79,7 +79,7 @@
               <div class="col-lg-3 col-sm-6 col-12">
                 <div class="form-group">
                   <label>File Image</label>
-                  <input type="file" name="image[]" class="form-control" multiple />
+                  <input type="file" name="image" class="form-control" multiple />
                   @if($data->image)
                     <div class="mt-2">
                       @foreach(explode(',', $data->image) as $image)
