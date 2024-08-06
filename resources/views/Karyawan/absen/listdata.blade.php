61a71e838a06b566b6e883e2ca9c805089666fa8
riskiai <riskiahmadiilham@gmail.com>
2024-08-07 00:48:38 +0700

    Update Data Penambahan Fitu Report Tgl 7 Agustus 2024

--- resources/views/Karyawan/absen/listdata.blade.php
+++ resources/views/Karyawan/absen/listdata.blade.php
@@ -20,12 +20,12 @@
         @include('Karyawan.absen.components_absen.navbar')
         <div class="table-top mt-3">
           <div class="search-set">
-            <div class="search-path">
+            {{-- <div class="search-path">
               <a class="btn btn-filter" id="filter_search">
                 <img src="{{ asset('assets/img/icons/filter.svg') }}" alt="img" />
                 <span><img src="{{ asset('assets/img/icons/closes.svg') }}" alt="img" /></span>
               </a>
-            </div>
+            </div> --}}
             <div class="search-input">
               <a class="btn btn-searchset">
                 <img src="{{ asset('assets/img/icons/search-white.svg') }}" alt="img" />
